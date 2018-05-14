18de6776f2d07b982006ba7a0ac5f545bbf1f8b6
RogerMinemu <roger@minemu.es>
2018-05-14 19:34:19 +0200

    JsonReader introduction

---
+++ src/main/java/JsonReader.java
@@ -0,0 +1,18 @@
+import com.google.gson.*;
+
+public class JsonReader {
+
+	private JsonObject json;
+	
+	public JsonReader(String ruta)
+	{
+		json = new JsonParser().parse(ruta + "'\'config.json").getAsJsonObject();
+	}
+	
+	public String getField(String field)
+	{
+		return json.get(field).getAsString();
+	}
+	
+	
+}
