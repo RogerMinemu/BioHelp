99804fb69f222371d98d2f28ecb8bbc340c1f651
RogerMinemu <roger@minemu.es>
2018-05-14 19:46:20 +0200

    Main.java clean

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -10,11 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("https://github.com/RogerMinemu/BioHelp");
-		
-		int cores = Runtime.getRuntime().availableProcessors();
-		
-		ArrayList<DiscordThread>ThD = new ArrayList<DiscordThread>(cores);
-		ArrayList<TelegramThread>ThT = new ArrayList<TelegramThread>(cores);
 		
 		//Telegram Init
 	      ApiContextInitializer.init();
@@ -27,16 +19,6 @@ public class Main {
 	          e.printStackTrace();
 	      }
 		
-		
-		Scanner sc = new Scanner(System.in);
-		String input;
-		
-		do {
-			System.out.println("Some testing input: ");
-			input = sc.nextLine();
-			
-			System.out.println(Similarity.compare("bases de datos de metabolomica", input));
-		}while(input != "salir");
 
 	}
 
