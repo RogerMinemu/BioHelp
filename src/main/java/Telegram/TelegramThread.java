2c92a043f685514f663cb2a3c3d3154a6a111228
Roger Lebron <roger@minemu.es>
2018-05-15 13:17:19 +0200

    Telegram Thread partial implementation

--- src/main/java/Telegram/TelegramThread.java
+++ src/main/java/Telegram/TelegramThread.java
@@ -2,26 +2,38 @@ package Telegram;
 
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.api.objects.Update;
 
 import Holders.BioData;
 
-public class TelegramThread extends Thread
-{
+import TextUtils.Similarity;
+
+public class TelegramThread extends Thread {
 	private Update update;
 	private Vector<BioData> bioData;
+	Logger log = Logger.getLogger("Telegram Thread");
 
-	public TelegramThread(Update update, Vector<BioData> bioData)
-	{
+	public TelegramThread(Update update, Vector<BioData> bioData) {
 		this.update = update;
 		this.bioData = bioData;
 	}
 
-	public void run()
-	{
-		String userMessage = this.update.getMessage().getText();
+	public void run() {
+		String input = this.update.getMessage().getText();
+
+		int simpos = 0;
+		double simint = 0;
 		
-		
-		
+		for (int i = 0; i < bioData.size(); i++) {
+			if (simint < Similarity.compare(input, bioData.get(i).question)) {
+				simpos = i;
+				simint = Similarity.compare(input, bioData.get(i).question);
+				log.info("New veracity record: " + simint);
+			}
+		}
+		//Aqui falta responder al usuario con el asnwer marcado abajo
+		log.info(bioData.get(simpos).answer);
+
 	}
 }
